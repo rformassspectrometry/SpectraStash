01d8f615b2df74ef8b07725a51110b7e507f8761
Johannes Rainer <johannes.rainer@gmail.com>
2026-06-23 08:27:13 +0200

    ci: try to fix installation error on macOS

---
+++ tests/testthat/test_zzz.R
@@ -0,0 +1,25 @@
+test_that("test .onLoad", {
+    ## collect options before and after loading the package
+    ## use different R subprocesses based on loading context
+    option_output <- {
+        if (testthat:::in_rcmd_check() || testthat:::in_covr()) {
+
+            callr::r(function() {
+                preOpts <- options()
+                library(SpectraStash)
+                expect_no_error(SpectraStash:::.onLoad())
+                postOpts <- options()
+                list(preOpts = preOpts, postOpts = postOpts)
+            })
+        } else if (!testthat:::in_rcmd_check()) {
+            callr::r(function() {
+                preOpts <- options()
+                pkgload::load_all()
+                expect_no_error(SpectraStash:::.onLoad())
+                postOpts <- options()
+                list(preOpts = preOpts, postOpts = postOpts)
+            })
+        }
+    }
+    expect_type(option_output, "list")
+})
